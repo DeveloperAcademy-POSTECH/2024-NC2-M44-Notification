8e5f2f23ab9cd1f4d551d433b28846dcfbf95986
dgh06175 <dgh06175@gmail.com>
2024-06-13 11:31:52 +0900

    inital ios project

---
+++ DiaryNotification/DiaryNotification/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiaryNotification
+//
+//  Created by 이상현 on 6/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiaryNotification/DiaryNotification/DiaryNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryNotificationApp.swift
+//  DiaryNotification
+//
+//  Created by 이상현 on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct DiaryNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
