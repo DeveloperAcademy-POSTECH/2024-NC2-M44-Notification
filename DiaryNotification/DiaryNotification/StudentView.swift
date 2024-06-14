c950a9e01bdf7d2e0dbd813868ff64e59de104d6
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-14 10:07:46 +0900

    Feat: create TextField in StudentView

--- DiaryNotification/DiaryNotification/StudentView.swift
+++ DiaryNotification/DiaryNotification/StudentView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct StudentView: View {
+    @State private var diaryInput = ""
     var body: some View {
-        Text("Student View")
+        VStack {
+            TextField("diary", text: $diaryInput, axis: .vertical)
+                .multilineTextAlignment(.leading)
+                .font(.title)
+                .frame(maxHeight: .infinity)
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .foregroundColor(.customYellow.opacity(0.2))
+                )
+                .padding(.vertical)
+            Spacer()
+            Button(action: {
+                print(diaryInput)
+            }, label: {
+                HStack {
+                    Spacer()
+                    Text("일기장 제출하기")
+                        .font(.title)
+                    Spacer()
+                }
+                .padding(.vertical)
+            })
+            .tint(.customYellow)
+            .buttonStyle(.borderedProminent)
+        }
+        .padding()
     }
 }
 
