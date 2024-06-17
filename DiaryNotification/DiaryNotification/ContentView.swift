34c1e453cdf61e87dea848589d09bf9262ad4d94
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-17 10:59:03 +0900

    Feat: return Student or TeacherView in ContenView

--- DiaryNotification/DiaryNotification/ContentView.swift
+++ DiaryNotification/DiaryNotification/ContentView.swift
@@ -13,21 +13,20 @@ struct ContentView: View {
     @State private var isDiarySubmitted = false
     let users = ["학생", "선생님"]
     var body: some View {
-//        VStack {
-//            Picker("user", selection: $currentView) {
-//                ForEach(users, id: \.self) { user in
-//                    Text("\(user)")
-//                }
-//            }
-//            .tint(.black)
-//            if currentView == "학생" {
-//                StudentView(currentView: $currentView, diaryInput: $diary, isDiarySubmitted: $isDiarySubmitted)
-//            } else {
-//                TeacherView(currentView: $currentView, diary: $diary, isDiarySubmitted: $isDiarySubmitted)
-//            }
-//        }
-//        .padding()
-        NotificationTestView()
+        VStack {
+            Picker("user", selection: $currentView) {
+                ForEach(users, id: \.self) { user in
+                    Text("\(user)")
+                }
+            }
+            .tint(.black)
+            if currentView == "학생" {
+                StudentView(currentView: $currentView, diaryInput: $diary, isDiarySubmitted: $isDiarySubmitted)
+            } else {
+                TeacherView(currentView: $currentView, diary: $diary, isDiarySubmitted: $isDiarySubmitted)
+            }
+        }
+        .padding()
     }
 }
 
