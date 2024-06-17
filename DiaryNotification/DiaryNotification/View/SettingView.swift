fd4a651d78e855cd23ef0f6fb0709c7070ada3d0
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-17 11:47:59 +0900

    Design: set title and body of notification

--- DiaryNotification/DiaryNotification/View/SettingView.swift
+++ DiaryNotification/DiaryNotification/View/SettingView.swift
@@ -15,7 +15,7 @@ struct SettingView: View {
             Spacer()
             DatePicker("일기 알림 시간: ", selection: $selectedDate, in: Date()...)
             Button("Send a notification at time") {
-                notify.sendNotification(date: selectedDate, type: "calendar", title: "Date Notification", body: "This is calendar notification")
+                notify.sendNotification(date: selectedDate, type: "calendar", title: "일기 쓸 시간!", body: "오늘 일기를 쓸 시간이에요. 알림을 클릭하여 일기를 써 볼까요?")
             }
             Spacer()
             Text("Not working?")
