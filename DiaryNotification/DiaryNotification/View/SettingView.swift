667bb218234228b039ebd847c4297de1fe5a12d7
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-17 11:56:37 +0900

    Feat: set SettingView style

--- DiaryNotification/DiaryNotification/View/SettingView.swift
+++ DiaryNotification/DiaryNotification/View/SettingView.swift
@@ -11,17 +11,25 @@ struct SettingView: View {
     @State private var selectedDate = Date()
     let notify = NotificationHandler()
     var body: some View {
-        VStack {
+        VStack(spacing: 30) {
             Spacer()
-            DatePicker("일기 알림 시간: ", selection: $selectedDate, in: Date()...)
-            Button("Send a notification at time") {
+            DatePicker("일기 쓸 시간:", selection: $selectedDate, in: Date()...)
+                .font(.title)
+            Button("설정한 시간에 알림 받기") {
                 notify.sendNotification(date: selectedDate, type: "calendar", title: "일기 쓸 시간!", body: "오늘 일기를 쓸 시간이에요. 알림을 클릭하여 일기를 써 볼까요?")
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title)
+            .tint(.customYellow)
             Spacer()
-            Text("Not working?")
-//            Button("Request permissions") {
+            Text("알림이 안 보이나요?")
+            Button("알림 권한 허용하기") {
+                print("set alarm permission")
 //                notify.askPermission()
-//            }
+            }
+            .font(.title)
+            .buttonStyle(.bordered)
+            .tint(.customYellow)
         }
         .padding()
     }
